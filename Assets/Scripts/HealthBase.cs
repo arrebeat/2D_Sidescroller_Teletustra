d27ce5ce27efd3f530a052049c166ceb04dd1b2f
arrebeat <andregtcolares@gmail.com>
2024-07-11 14:32:44 -0300

    HealthBase, EnemyBase, Player death added;

---
+++ Assets/Scripts/HealthBase.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBase : MonoBehaviour
+{
+    private PlayerController player;
+    public int MaxHealth;
+    public int CurrentHealth;
+
+
+    void Awake()
+    {
+        player = GetComponentInParent<PlayerController>();
+        CurrentHealth = MaxHealth;
+    }
+    
+    public void TakeDamage(int damage)
+    {
+        if (player.HasDied) return;
+
+        CurrentHealth -= damage;
+
+        if (CurrentHealth <= 0)
+        {
+            player.Die();
+        }
+    }
+}
